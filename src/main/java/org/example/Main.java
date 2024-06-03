3bdc2ad22dc2375ff0256be8a638c08d4215585a
MindaugasRo <mindaugas.rocys@live.com>
2024-06-03 20:00:52 +0300

    Main klasėje sukurtas CASE metodas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,40 +7,75 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Author.printAuthors();
+        //Author.printAuthors();
 
-//        ArrayList<Author> authors = Author.selectAll();
-//        System.out.println(Author.selectAll());
-//        for (Author author : authors ) {
-//           System.out.println(author);
-//           //System.out.println(author.getId()  + " " + author.getName() + " " + author.getSurName());
-//        }
-//        Author a = Author.findById(8);
-//        System.out.println(a);
-//        a.setName("Valdemaras");
-//        a.setSurName("Vunderkindas");
-//        a.update();
-//        a = Author.findById(8);
-//        System.out.println(a);
-//        Author.delete(112);
-//        Author.create("Valdemaras", "Jaunesnysis Indigo-Vunderkindas");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            menuOptionsMessage();
+            int optionInput = numberInput(sc);
+            sc.nextLine();
+
+            switch (optionInput) {
+                case 1:
+                    Author.printAuthors();
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        }
     }
 
 
-    public static Connection connect(){
+    public static Connection connect() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ats_231023_library","root","");
-        }catch (Exception e){
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ats_231023_library", "root", "");
+        } catch (Exception e) {
             System.out.println("Failed to connect to database");
         }
 
         return connection;
+    }
+
+    public static void menuOptionsMessage() {
+        System.out.println();
+        System.out.println("---------------");
+        System.out.println(" 1 - Rodyti autorių sąrašą");
+        System.out.println(" 2 - Įvesti naują autorių");
+        System.out.println(" 3 - Redaguoti autorių sąrašą");
+        System.out.println(" 4 - Ištrinti autorių");
+        System.out.println(" 5 - Stabdyti programą");
+        System.out.println("---------------");
+    }
+
+    public static int numberInput(Scanner sc) {
+        while (true) {
+            try {
+                return sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Įveskite savo pasirinkimą skaitmenimis");
+                sc.nextLine();
+            }
+        }
+    }
+
+    public static void programExit() {
+        System.out.println("Programa uždaryta");
+        System.exit(1);
     }
 
 
